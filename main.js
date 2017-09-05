9c083cce84014c0be3b05670cdc7ad9d313fc08c
clarkgh <demolisho@hotmail.com>
2017-09-05 13:05:35 -0700

    added event listener for autocomplete

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 var initMap = () => {
-  setAutocomplete();
+  const input = document.getElementById('placesAutocomplete');
+  setAutocomplete( input );
+  preventSubmitOnEnter( input );
   setMap();
 }
-
-function setAutocomplete() {
-  var input = document.getElementById('placesAutocomplete'),
-    autocomplete = new google.maps.places.Autocomplete(input);
+// refactor to jquery on all
+function setAutocomplete( input ) {
+  var autocomplete = new google.maps.places.Autocomplete( input );
 }
 
 function setMap() {
-  var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-  };
-  var map= new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  const mapProp = {
+    center: new google.maps.LatLng( 37.7749, -122.4194 ),
+    zoom: 15,
+  },
+    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+function preventSubmitOnEnter ( input ) {
+  google.maps.event.addDomListener(input, 'keydown', function( event ) { 
+    if (event.keyCode === 13) { 
+        event.preventDefault(); 
+    }
+  });
 }
