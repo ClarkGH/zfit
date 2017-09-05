4563a7f0151363c6ff4c4fba8c642bef5c778a32
clarkgh <demolisho@hotmail.com>
2017-09-05 12:28:57 -0700

    map now loading

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-var initAutocomplete = () => {
+var initMap = () => {
+  setAutocomplete();
+  setMap();
+}
+
+function setAutocomplete() {
   var input = document.getElementById('placesAutocomplete'),
     autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+function setMap() {
+  var mapProp = {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  var map= new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
