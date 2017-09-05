feb31d4a87dd404772e4ca6ffed515deec2efea6
clarkgh <demolisho@hotmail.com>
2017-09-05 14:24:15 -0700

    map/markers on load, need to connect search bar to map now

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-var initMap = () => {
-  const input = document.getElementById('placesAutocomplete');
-  setAutocomplete( input );
-  preventSubmitOnEnter( input );
-  setMap();
-}
-// refactor to jquery on all
-function setAutocomplete( input ) {
-  var autocomplete = new google.maps.places.Autocomplete( input );
+var map,
+  infowindow,
+  initialize = () => {
+    const input = document.getElementById( 'placesAutocomplete' );
+    initMap();
+    initAutocomplete( input );
+    preventSubmitOnEnter( input );
+  };
+
+function initMap() {
+  var center = new google.maps.LatLng( 37.7749, -122.4194 ),
+    mapProp = {
+      center: center,
+      zoom: 13,
+    };
+  map = new google.maps.Map( document.getElementById( 'googleMap' ), mapProp );
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService( map );
+
+  service.nearbySearch({
+    location: center,
+    radius: 500,
+    type: ['bakery']
+  }, mapsCallback);
 }
 
-function setMap() {
-  const mapProp = {
-    center: new google.maps.LatLng( 37.7749, -122.4194 ),
-    zoom: 15,
-  },
-    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+function initAutocomplete( input ) {
+  const autocomplete = new google.maps.places.Autocomplete( input );
 }
+
 
 function preventSubmitOnEnter ( input ) {
   google.maps.event.addDomListener(input, 'keydown', function( event ) { 
@@ -23,4 +35,25 @@ function preventSubmitOnEnter ( input ) {
         event.preventDefault(); 
     }
   });
+}
+
+function mapsCallback( results, status ) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker( place ) {
+  var placeLocation = place.geometry.location,
+    marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener( marker, 'click', function() { //anon arrow function will not work in this case since we're using this function's scope
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
 }
